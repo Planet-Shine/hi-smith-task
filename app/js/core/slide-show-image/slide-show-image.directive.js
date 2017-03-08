0badb6e19572568233af6356ac9a24edd457b53c
PavelOfficial <pavelofficial@ya.ru>
2017-03-08 13:50:05 +0300

    fixes

--- app/js/core/slide-show-image/slide-show-image.directive.js
+++ app/js/core/slide-show-image/slide-show-image.directive.js
@@ -5,12 +5,13 @@ angular
     .directive('slideShowImage', function () {
         return function (scope, element) {
             element.on('load', function () {
+                var img = element[0];
+                img.style.height = "";
                 setTimeout(function () {
-                    var img = element[0];
                     if (img.clientHeight >  img.clientWidth) {
                         img.style.height = parseInt(window.innerHeight * 0.8 , 10) + 'px';
                     }
-                }, 0);
+                }, 50);
             });
         }
     });
